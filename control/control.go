2341cfb10dead710b85d54dc8e3b263f27a5aedf
acorn <776334655@qq.com>
2019-03-08 22:07:36 +0800

    添加接口

--- control/control.go
+++ control/control.go
@@ -1,3 +1,22 @@
 package control
 
 //https gin 控制层
+/*
+ *   /user/base/login
+ *   /user/base/logout
+ *   /user/base/info
+ *   /user/base/valid
+ *   /user/base/register
+ *
+ *   /user/auth/has_role
+ *   /user/auth/has_permission
+ *   /user/auth/roles
+ *   /user/auth/permissions
+ *   /user/auth/app_keys
+ *
+ *   /manager/role/new_role             给系统添加新角色
+ *   /manager/role/new_permission       给角色添加permission
+ *	 /manager/permission/new_permission 给系统添加新权限
+ *   /manager/user/new_role             给用户添加新角色
+ *   /manager/app/new_keys              给app用户添加新的key
+ */
