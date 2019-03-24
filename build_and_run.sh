809d8fe4560b7bcb4a5f9e56db2d53bf81e88abb
acorn <776334655@qq.com>
2019-03-24 16:39:28 +0800

    添加docker支持

--- build_and_run.sh
+++ build_and_run.sh
@@ -9,7 +9,7 @@ docker build -t lico603/lico_user:$BUILD_NUMBER .
 
 docker stop lico_user
 docker rm lico_user
-docker run --name lico_user -p 1000:8080 lico603/lico_user:$BUILD_NUMBER
+docker run --name lico_user -d -p 1000:8080 lico603/lico_user:$BUILD_NUMBER
 
 echo "end"
 
