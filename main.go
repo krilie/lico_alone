d1a065b7817dfda131d063ea5e9944703a397368
acorn <776334655@qq.com>
2019-04-27 10:45:30 +0800

    alone

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"github.com/fvbock/endless"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/krilie/lico_alone/common/config"
 	"github.com/krilie/lico_alone/common/log"
 	"github.com/krilie/lico_alone/control"
+	"golang.org/x/net/context"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -14,19 +18,46 @@ func main() {
 	control.LocalRouter.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World")
 	})
-
+	// 开始
+	srv := &http.Server{
+		Addr:    ":" + config.GetString("service.port"),
+		Handler: control.LocalRouter,
+	}
 	//是否有ssl.public_key ssl.private_key
 	pubKey := config.GetString("ssl.public_key")
 	priKey := config.GetString("ssl.private_key")
 	if pubKey == "" || priKey == "" {
-		if err := endless.ListenAndServe(":"+config.GetString("service.port"), control.LocalRouter); err != nil {
-			log.Warningln(err)
-			return
-		}
+		go func() {
+			if err := srv.ListenAndServe(); err != nil {
+				log.Warningln(err)
+				return
+			}
+		}()
 	} else {
-		if err := endless.ListenAndServeTLS(":"+config.GetString("service.port"), pubKey, priKey, control.LocalRouter); err != nil {
-			log.Warningln(err)
-			return
+		go func() {
+			if err := srv.ListenAndServeTLS(pubKey, priKey); err != nil {
+				log.Warningln(err)
+				return
+			}
+		}()
+	}
+
+	// 关闭服务器
+	c := make(chan os.Signal, 0)
+	signal.Notify(c)
+	for {
+		// Block until a signal is received.
+		s := <-c
+		fmt.Println("Got signal:", s) //Got signal: terminated
+		if s == syscall.SIGINT || s == syscall.SIGTERM {
+			// shutdown
+			shutdown := srv.Shutdown(context.Background())
+			if shutdown != nil {
+				log.Error(shutdown)
+			} else {
+				log.Info("end of service...")
+			}
 		}
 	}
+
 }
