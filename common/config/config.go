cb6b309c9b23624fbb7db3c9e68123761eb3b112
acorn <776334655@qq.com>
2019-05-11 07:35:13 +0800

    buind and run

--- common/config/config.go
+++ common/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/krilie/lico_alone/common/log"
 	"github.com/spf13/viper"
+	"strings"
 )
 
 type config struct {
@@ -13,6 +14,13 @@ var Conf = &config{}
 
 func init() {
 	Conf.v = viper.New()
+
+	//读取环境变量值
+	Conf.v.SetEnvPrefix("LICO")
+	Conf.v.AutomaticEnv()
+	replacer := strings.NewReplacer(".", "_")
+	Conf.v.SetEnvKeyReplacer(replacer)
+
 	//设置配置文件的名字
 	Conf.v.SetConfigName("config")
 	//添加配置文件所在的路径,注意在Linux环境下%GOPATH要替换为$GOPATH
@@ -29,12 +37,9 @@ func init() {
 	Conf.v.SetDefault("db.max_open_conn", 1)
 	Conf.v.SetDefault("db.max_idle_conn", 1)
 	Conf.v.SetDefault("db.conn_max_left_time", 3600*7)
-	Conf.v.SetDefault("jwt.normal_exp_duration", 3600*24*30)
-	//ssl key
-	//Conf.v.SetDefault("ssl.public_key", "public.key")
 	Conf.v.SetDefault("ssl.public_key", "")
-	//Conf.v.SetDefault("ssl.private_key", "private.key")
 	Conf.v.SetDefault("ssl.private_key", "")
+	Conf.v.SetDefault("jwt.normal_exp_duration", 3600*24*30)
 	Conf.v.SetDefault("jwt.hs256_key", "E5Vsfs#$afasdrtfawe*^&%(")
 
 	if err := Conf.v.ReadInConfig(); err != nil {
