067e640239353296e653fb1201efd32ba0fa049a
acorn <776334655@qq.com>
2019-05-12 16:57:34 +0800

    buind and run

--- common/time_util/time_util.go
+++ common/time_util/time_util.go
@@ -5,19 +5,24 @@ import "time"
 //func GetNowUnix() int64 {
 //	return time.Now().Unix()
 //}
+var cstZone *time.Location
+
+func init() {
+	cstZone = time.FixedZone("CST", 8*3600) // 东八
+}
 
 func GetNowTimeString() string {
-	return time.Now().Format("2006-01-02 15:04:05")
+	return time.Now().In(cstZone).Format("2006-01-02 15:04:05")
 }
 func GetNowTimeStringFormat(format string) string {
-	return time.Now().Format(format)
+	return time.Now().In(cstZone).Format(format)
 }
 
 func GetTimeNow() *time.Time {
-	timeN := time.Now()
+	timeN := time.Now().In(cstZone)
 	return &timeN
 }
 
 func GetTimeString(unix int64) string {
-	return time.Unix(unix, 0).Format("2006-01-02 15:04:05")
+	return time.Unix(unix, 0).In(cstZone).Format("2006-01-02 15:04:05")
 }
