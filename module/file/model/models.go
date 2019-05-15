737e5b5a398b6c9b5ab1cad7fa36fdbbbae7f29b
acorn <776334655@qq.com>
2019-05-15 22:05:37 +0800

    连接时区

--- module/file/model/models.go
+++ module/file/model/models.go
@@ -5,10 +5,10 @@ import "github.com/krilie/lico_alone/common/common_struct"
 // 上传的文件的内容
 type File struct {
 	common_struct.DbHandler
-	Path    string `gorm:"type:varchar(200);unique_index;not null"`
-	Suffix  string `gorm:"type:varchar(50)"`
-	BizType string `gorm:"type:varchar(50)"`
-	Size    int    `gorm:"type:int;not null"`
+	ObjKey      string `gorm:"type:varchar(200);unique_index;not null"`
+	ContentType string `gorm:"type:varchar(50)"`
+	BizType     string `gorm:"type:varchar(50)"`
+	Size        int    `gorm:"type:int;not null"`
 }
 
 func (File) TableName() string {
--- module/file/service/operation.go
+++ module/file/service/operation.go
@@ -4,10 +4,12 @@ import (
 	"github.com/krilie/lico_alone/common/common_struct/errs"
 	"github.com/krilie/lico_alone/common/context_util"
 	"github.com/krilie/lico_alone/common/uuid_util"
+	"github.com/krilie/lico_alone/module/file/model"
 	"github.com/minio/minio-go"
 	"io"
 	"mime/multipart"
 	"net/http"
+	"time"
 )
 
 func UploadFile(ctx *context_util.Context, userId, fileName string, file multipart.File, size int64) (string, *errs.Error) {
@@ -26,26 +28,51 @@ func UploadFile(ctx *context_util.Context, userId, fileName string, file multipa
 	objName := uuid_util.GetUuid() + fileName
 	userMate := make(map[string]string)
 	userMate["user_id"] = userId
+	tx := model.Db.Begin()
+	fileS := model.File{}
+	fileS.ID = uuid_util.GetUuid()
+	fileS.CreateTime = time.Now()
+	fileS.ObjKey = objName
+	fileS.ContentType = contentType
+	//fileS.BizType = ""
+	fileS.Size = int(size)
+	err = tx.Create(fileS).Error
+	if err != nil {
+		tx.Rollback()
+		return "", errs.ErrInternal.NewWithMsg(err.Error())
+	}
 	n, err := minioClient.PutObject(ossBucket, objName, file, size, minio.PutObjectOptions{ContentType: contentType, UserMetadata: userMate})
 	if err != nil {
+		tx.Rollback()
 		_ = minioClient.RemoveIncompleteUpload(ossBucket, objName) // 删除可能存在的不完整文件
 		return "", errs.ErrInternal.NewWithMsg(err.Error())
 	} else if n != size {
+		tx.Rollback()
 		_ = minioClient.RemoveIncompleteUpload(ossBucket, objName) // 删除可能存在的不完整文件
 		return "", errs.ErrInternal.NewWithMsg("un completed upload please check")
 	} else {
+		tx.Commit()
 		return objName, nil
 	}
 }
 
 func DeleteFile(ctx *context_util.Context, userId, filePath string) *errs.Error {
+	tx := model.Db.Begin()
+	e := tx.Delete(model.File{}, model.File{ObjKey: filePath}).Error
+	if e != nil {
+		tx.Rollback()
+		return errs.ErrInternal.NewWithMsg(e.Error())
+	}
 	minioClient, err := minio.New(ossEndPoint, ossKey, ossSecret, true)
 	if err != nil {
+		tx.Rollback()
 		return errs.ErrInternal.NewWithMsg(err.Error())
 	}
 	err = minioClient.RemoveObject(ossBucket, filePath)
 	if err != nil {
+		tx.Rollback()
 		return errs.ErrInternal.NewWithMsg(err.Error())
 	}
+	tx.Commit()
 	return nil
 }
