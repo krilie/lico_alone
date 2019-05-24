14551c2eeb05f7d35644e8a5e915a1a17886a85b
lhh <776334655@qq.com>
2019-05-24 09:55:44 +0800

    user register

--- module/userbase/user/user_register.go
+++ module/userbase/user/user_register.go
@@ -14,13 +14,17 @@ import (
 
 //用户注册，注册，normal用户注册
 func (User) Register(ctx *context_util.Context, loginName string, password string) error {
-	//检查密码与用户名
+	// 检查密码与用户名格式
 	if !(validator_util.IsLoginName(loginName) && validator_util.IsPassword(password)) {
 		log.Infoln("user loginName or password format error.")
 		return errs.ErrParam.NewWithMsg("用户名不能以数字开头&密码至少8位")
 	}
-	//插入用户数据
+	// 检查密码与用户名的存在
 	var user model.User
+	if e := model.Db.Find(&user, "login_name = ?", loginName).Error; e == nil {
+		return errs.ErrParam.NewWithMsg("此名称已被使用")
+	}
+	// 插入用户数据
 	user.ID = uuid_util.GetUuid()
 	user.LoginName = loginName
 	user.NickName = loginName
