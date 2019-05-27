6d4d585ffa5fff38cd7398c478ba2a62c966397a
acorn <1197829331@qq.com>
2019-05-27 19:17:39 +0800

    bill

--- module/account/service/add_bill.go
+++ module/account/service/add_bill.go
@@ -1,1 +1,11 @@
 package service
+
+import (
+	"github.com/krilie/lico_alone/common/context"
+	"github.com/krilie/lico_alone/module/account/model"
+	"github.com/shopspring/decimal"
+)
+
+func (Account) AddBill(ctx *context.Context, userId, note, image string, amount decimal.Decimal, detail []model.BillDetail) {
+
+}
