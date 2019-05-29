7bfe3895a609b4be76ec64627dbd8123b6bc5093
lhh <776334655@qq.com>
2019-05-29 09:48:08 +0800

    时区

--- common/time_util/date_time_test.go
+++ common/time_util/date_time_test.go
@@ -3,14 +3,12 @@ package time_util
 import (
 	"fmt"
 	"testing"
-	time2 "time"
+	"time"
 )
 
 func TestGetBeijingZeroTime(t *testing.T) {
-	fmt.Println(time2.Now())
-	time := time2.Now()
-	zeroTime := GetBeijingZeroTime(time)
-	fmt.Println(zeroTime)
-	fmt.Println(GetBeijingMonthStartTime(time2.Now()))
-	fmt.Println(GetBeijingLastDateOfMonth(time2.Now()))
+	fmt.Println(time.Now())
+	fmt.Println(GetBeijingZeroTime(time.Now()))
+	fmt.Println(GetBeijingMonthStartTime(time.Now()))
+	fmt.Println(GetBeijingLastDateOfMonth(time.Now()))
 }
