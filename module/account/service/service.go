723db0502af3bba1286ddc55c990fd164e67d9ee
acorn <1197829331@qq.com>
2019-05-29 20:32:34 +0800

    获取 account 信息

--- module/account/service/service.go
+++ module/account/service/service.go
@@ -1,3 +1,22 @@
 package service
 
+import (
+	"github.com/krilie/lico_alone/common/context"
+	"github.com/krilie/lico_alone/module/account/model"
+	"github.com/krilie/lico_alone/module/account/pojo"
+	"github.com/shopspring/decimal"
+	"time"
+)
+
 type Account struct{}
+
+type Accounter interface {
+	DeleteBill(ctx *context.Context, billId string) error
+	GetAccountHistory(ctx *context.Context, start, end time.Time, userId, AccountId, note string) ([]model.BillDetail, error)
+	GetAccountSummaryInfo(ctx *context.Context, userId string) ([]*pojo.AccountInfo, error)
+	AddBill(ctx *context.Context, userId, note, image string, amount decimal.Decimal, detail []pojo.BillDetail) (string, error)
+	AddAccount(ctx *context.Context, userId, name, num, description, image string, balance decimal.Decimal) (string, error)
+	DeleteAccount(ctx *context.Context, accountId string) error
+	GetMonthSummary(ctx *context.Context, userId string, time time.Time) (*pojo.AccountSummary, error)
+	GetTimeZoneSummary(ctx *context.Context, userId string, timeStart, timeEnd time.Time) (*pojo.AccountSummary, error)
+}
