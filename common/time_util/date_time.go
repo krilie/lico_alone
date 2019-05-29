6dd1d78df0b7168213c80cb66ce88868f5a921f8
lhh <776334655@qq.com>
2019-05-29 09:51:55 +0800

    时区

--- common/time_util/date_time.go
+++ common/time_util/date_time.go
@@ -12,7 +12,7 @@ func GetBeijingMonthStartTime(time time.Time) time.Time {
 
 // 获取传入的时间所在月份的最后一天，即某月最后一天的0点。如传入time.Now(), 返回当前月份的最后一天0点时间。
 func GetBeijingLastDateOfMonth(d time.Time) time.Time {
-	return GetBeijingMonthStartTime(d).In(BeijingZone).AddDate(0, 1, 0)
+	return GetBeijingMonthStartTime(d).In(BeijingZone).AddDate(0, 1, 0).Add(-time.Second)
 }
 
 // 获取零点时间
