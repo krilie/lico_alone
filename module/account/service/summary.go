9a1417420e56edcf7dd6fb2d65dd36b27d9926e6
acorn <1197829331@qq.com>
2019-05-29 20:15:37 +0800

    获取 account 信息

--- module/account/service/summary.go
+++ module/account/service/summary.go
@@ -64,7 +64,7 @@ func (Account) GetTimeZoneSummary(ctx *context.Context, userId string, timeStart
 // 获取一个账户的account信息
 func (Account) GetAccountHistory(ctx *context.Context, start, end time.Time, userId, AccountId, note string) ([]model.BillDetail, error) {
 	billDetails := make([]model.BillDetail, 0, 4)
-	if e := model.Db.Where("create_time between ? and ? and user_id=?", start, end, userId).Find(billDetails).Error; e != nil {
+	if e := model.Db.Where("create_time between ? and ? and account_id=?", start, end, AccountId).Find(billDetails).Error; e != nil {
 		return nil, errs.ErrInternal.NewWithMsg(e.Error())
 	}
 	return billDetails, nil
