550cfb187549262d0e23d165a00cb1a8d62950fd
lhh <776334655@qq.com>
2019-06-20 11:32:56 +0800

    account

--- common/comlog/log.go
+++ common/comlog/log.go
@@ -1,35 +1,50 @@
-package comlog
+package brlog
 
 import (
 	"context"
-	"github.com/krilie/lico_alone/common/comstruct/errs"
 	context2 "github.com/krilie/lico_alone/common/context"
 	"github.com/sirupsen/logrus"
 	"os"
 )
 
+var (
+	app_name    = "app_name"
+	app_version = "app_version"
+	app_host    = "app_host"
+	trace_id    = "trace_id"
+	client_id   = "client_id"
+	user_id     = "user_id"
+	module      = "module"
+	function    = "function"
+	stack       = "stack"
+)
+
 var Log = logrus.NewEntry(logrus.New())
 
 func init() {
-	Log.Logger.SetFormatter(&logrus.JSONFormatter{})
+	Log.Logger.SetFormatter(&logrus.JSONFormatter{TimestampFormat: "2006-01-02T15:04:05.000Z07:00"})
 	Log.Logger.SetLevel(logrus.DebugLevel)
+	//
+	//file, e := os.OpenFile("./log.txt", os.O_CREATE|os.O_APPEND, os.ModeAppend)
+	//if e != nil {
+	//	panic(e)
+	//	return
+	//}
+	Log.Logger.SetOutput(os.Stdout)
 	Log = Log.
-		WithField("app_name", os.Getenv("BR_APP_NAME")).
-		WithField("app_version", os.Getenv("BR_APP_VERSION")).
-		WithField("app_host", os.Getenv("HOST_NAME"))
+		WithField(app_name, os.Getenv("BR_APP_NAME")).
+		WithField(app_version, os.Getenv("BR_APP_VERSION")).
+		WithField(app_host, os.Getenv("HOST_NAME"))
 	Log.Infoln("log init ok")
 }
 
 // trace_id
-func NewLog(ctx context.Context, moduleName string) *logrus.Entry {
-	lctx := ctx.(*context2.Context)
-	if lctx == nil {
-		panic(errs.ErrInternal.NewWithMsg("can not get context on NewLog"))
-		return nil
-	}
+func NewLog(ctx context.Context, moduleName string, functionName string) *logrus.Entry {
+	bctx := ctx.(*context2.Context)
 	return Log.WithFields(logrus.Fields{
-		"trace_id":  lctx.GetTraceId(),
-		"client_id": lctx.GetClientId(),
-		"user_id":   lctx.GetUserId(),
-		"module":    moduleName})
+		trace_id:  bctx.GetTraceId(),
+		client_id: bctx.GetClientId(),
+		user_id:   bctx.GetUserId(),
+		module:    moduleName,
+		function:  functionName})
 }
