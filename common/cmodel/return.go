afd5f2a137256a27486aadc1a1c1e3933fbae85c
lhh <776334655@qq.com>
2019-10-09 11:08:41 +0800

    统一返回值

---
+++ common/cmodel/return.go
@@ -0,0 +1,54 @@
+package cmodel
+
+import (
+	"github.com/krilie/lico_alone/common/errs"
+)
+
+var StdSuccess = &CommonReturn{
+	Code:     200,
+	Message:  "success",
+	Detail:   nil,
+	Data:     nil,
+	PageInfo: nil,
+}
+
+type CommonReturn struct {
+	Code     int         `json:"code" swaggo:"true,错误码" example:"400"`
+	Message  string      `json:"message" swaggo:"true,错误信息" example:"错误信息"`
+	Detail   *string     `json:"detail,omitempty" swaggo:"false,错误的详细信息，用于排查错误"  example:"错误的详细信息，用于排查错误"` // 可由运行模式控制是否显示
+	Data     interface{} `json:"data,omitempty" `                                                          // 数据值
+	PageInfo *PageInfo   `json:"page_info,omitempty"`                                                      // 分页信息
+}
+
+func NewRet(err *errs.Err) *CommonReturn {
+	fullMsg := err.GetFullMsg()
+	return &CommonReturn{
+		Code:     err.Code,
+		Message:  err.Message,
+		Detail:   &fullMsg,
+		Data:     nil,
+		PageInfo: nil,
+	}
+}
+func NewRetFromErr(err error) *CommonReturn {
+	fullMsg := errs.GetErrMsg(err)
+	return &CommonReturn{
+		Code:     500,
+		Message:  err.Error(),
+		Detail:   &fullMsg,
+		Data:     nil,
+		PageInfo: nil,
+	}
+}
+func NewFailure(code int, msg string) *CommonReturn {
+	if msg == "" {
+		msg = "failure"
+	}
+	return &CommonReturn{
+		Code:     code,
+		Message:  msg,
+		Detail:   nil,
+		Data:     nil,
+		PageInfo: nil,
+	}
+}
