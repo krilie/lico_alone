066f8cf77ab5cfcfbac72f4a128b86fe4b8a5f09
lhh <776334655@qq.com>
2019-10-09 13:37:46 +0800

    测试

--- application/user-api/user_login_test.go
+++ application/user-api/user_login_test.go
@@ -2,6 +2,7 @@ package user_api
 
 import (
 	"context"
+	"fmt"
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/utils"
 	all_service "github.com/krilie/lico_alone/application/all-service"
 	"github.com/krilie/lico_alone/common/cdb"
@@ -18,10 +19,22 @@ func TestAppUser_UserRegister(t *testing.T) {
 func BenchmarkAppUser_UserRegister(b *testing.B) {
 	cdb.Db.LogMode(false)
 	user := NewAppUser(all_service.NewAllService(config.Cfg))
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		err := user.UserRegister(context.Background(), utils.GetUUID()[:20], "sss", "")
 		if err != nil {
 			b.Log(err)
 		}
 	}
+}
+
+// BenchmarkSprintf 对 fmt.Sprintf 函数进行基准测试
+func BenchmarkSprintf(b *testing.B) {
+	number := 10
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("%d", number)
+	}
+
 }
