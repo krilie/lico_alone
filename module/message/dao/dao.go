65aa747f18ca4a4c1a7e94fc478836b48ffd48a3
lhh <776334655@qq.com>
2019-10-28 18:44:37 +0800

    infra

---
+++ module/message/dao/dao.go
@@ -0,0 +1,30 @@
+package dao
+
+import (
+	"github.com/krilie/lico_alone/common/ccontext"
+	"github.com/krilie/lico_alone/common/cdb"
+	"github.com/krilie/lico_alone/common/clog"
+	"github.com/krilie/lico_alone/common/config"
+	"github.com/krilie/lico_alone/module/message/model"
+)
+
+func AutoMigrate(d *Dao) {
+	var log = clog.NewLog(ccontext.NewContext(), "alone.module.user.model", "init")
+	err := d.Db.AutoMigrate(new(model.MessageEmail), new(model.MessageSms)).Error
+	if err != nil {
+		panic(err)
+	}
+	log.Info("user dao init done")
+}
+
+type Dao struct {
+	*cdb.Dao
+}
+
+func NewDao(cfg config.DB) *Dao {
+	d := &Dao{
+		Dao: cdb.NewDao(cfg),
+	}
+	AutoMigrate(d)
+	return d
+}
