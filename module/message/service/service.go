ef553fa057c3ecd2ab04aa2b05b28a6ec45989cb
lhh <776334655@qq.com>
2019-10-28 18:46:10 +0800

    infra

---
+++ module/message/service/service.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"context"
+	"github.com/jinzhu/gorm"
+	"github.com/krilie/lico_alone/common/cdb"
+	"github.com/krilie/lico_alone/common/clog"
+	"github.com/krilie/lico_alone/common/config"
+	"github.com/krilie/lico_alone/module/message/dao"
+)
+
+type Service struct {
+	Dao *dao.Dao
+}
+
+func (a *Service) SetTx(ctx context.Context, tx *gorm.DB) (service cdb.Service, err error) {
+	var log = clog.NewLog(ctx, "module/file/service/service.go.set_tx", "SetTx")
+	log.Debug("new tx")
+	txDao, err := a.Dao.Begin(tx)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	return &Service{
+		Dao: &dao.Dao{Dao: txDao},
+	}, err
+}
+
+func (a *Service) GetDb(ctx context.Context) *gorm.DB {
+	return a.Dao.Db
+}
+
+func NewService(cfg config.Config) *Service {
+	return &Service{
+		Dao: dao.NewDao(cfg.DB),
+	}
+}
