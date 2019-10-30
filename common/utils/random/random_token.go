839772bd33670ffabf7de763a967c97976e3cdfd
acorn <1197829331@qq.com>
2019-10-30 20:48:05 +0800

    infra

--- common/utils/random/random_token.go
+++ common/utils/random/random_token.go
@@ -19,7 +19,17 @@ func GetAToken() string {
 
 //获取盐值
 func GetRandomNum(num uint) string {
-	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
-	vcode := fmt.Sprintf("%v", rnd.Int31n(1000000))
-	return vcode[:num]
+	if num <= 5 {
+		rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+		vcode := fmt.Sprintf("%v", rnd.Int31n(1000000))
+		return vcode[:num]
+	} else {
+		bytes := make([]byte, 0, num)
+		for i := uint(0); i < num; i++ {
+			rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+			b := rnd.Intn(9) + int(byte('0'))
+			bytes = append(bytes, byte(b))
+		}
+		return string(bytes)
+	}
 }
--- common/utils/random/random_token_test.go
+++ common/utils/random/random_token_test.go
@@ -7,5 +7,10 @@ func TestGetAToken(t *testing.T) {
 	t.Log("2: " + GetAToken())
 	t.Log("3: " + GetAToken())
 	t.Log("4: " + GetAToken())
+}
+
+func TestGetRandomNum(t *testing.T) {
 	t.Log(GetRandomNum(6))
+	t.Log(GetRandomNum(5))
+	t.Log(GetRandomNum(3))
 }
