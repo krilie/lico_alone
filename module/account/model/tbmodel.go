b85b9bab76974af9e5366b3dfdd6d129d2da9aca
lhh <776334655@qq.com>
2019-11-01 18:20:33 +0800

    fix bug

--- module/account/model/tbmodel.go
+++ module/account/model/tbmodel.go
@@ -25,11 +25,12 @@ func (AccountItem) TableName() string {
 
 type AccountBill struct {
 	cmodel.Model
-	UserId  string          `gorm:"type:varchar(32);not null" json:"user_id"`
-	Amount  decimal.Decimal `gorm:"type:decimal(14,2);not null;default 0"json:"amount"` // 发生额
-	IsValid bool            `gorm:"type:boolean;not null" json:"is_valid"`
-	Image   string          `gorm:"type:varchar(500);not null" json:"image"`
-	Note    string          `gorm:"type:varchar(100);default null" json:"note"`
+	PreOpLogIndex int64           `json:"pre_op_log_index" gorm:"column:pre_op_log_index"`
+	UserId        string          `gorm:"type:varchar(32);not null" json:"user_id"`
+	Amount        decimal.Decimal `gorm:"type:decimal(14,2);not null;default 0"json:"amount"` // 发生额
+	IsValid       bool            `gorm:"type:boolean;not null" json:"is_valid"`
+	Image         string          `gorm:"type:varchar(500);not null" json:"image"`
+	Note          string          `gorm:"type:varchar(100);default null" json:"note"`
 }
 
 func (AccountBill) TableName() string {
@@ -38,4 +39,25 @@ func (AccountBill) TableName() string {
 
 type AccountBillDetail struct {
 	cmodel.Model
+	BillId        string          `gorm:"type:varchar(32);not null" json:"bill_id"`
+	AccountItemId string          `gorm:"type:varchar(32);not null" json:"account_item_id"`
+	Amount        decimal.Decimal `gorm:"type:decimal(14,2);not null;default 0"json:"amount"` // 值 可正可负 负借 正贷
+}
+
+func (AccountBillDetail) TableName() string {
+	return "tb_account_bill_detail"
+}
+
+// 操作记录 可撤销最后一次记录不留痕迹
+type AccountOperatorLog struct {
+	cmodel.Model
+	OpIndex       int64           `json:"op_index" gorm:"column:op_index"` // 操作序号
+	BillId        string          `gorm:"type:varchar(32);not null" json:"bill_id"`
+	AccountItemId string          `gorm:"type:varchar(32);not null" json:"account_item_id"`
+	Amount        decimal.Decimal `gorm:"type:decimal(14,2);not null;default 0"json:"amount"` // 值 可正可负 负借 正贷
+	Message       string          `gorm:"type:varchar(500);not null" json:"message"`
+}
+
+func (AccountOperatorLog) TableName() string {
+	return "tb_account_operator_log"
 }
