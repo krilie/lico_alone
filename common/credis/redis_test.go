838a6450089d9011db8fad3c0df7a0033d774031
acorn <1197829331@qq.com>
2020-01-12 16:29:27 +0800

    redis

--- common/credis/redis_test.go
+++ common/credis/redis_test.go
@@ -16,4 +16,25 @@ func TestExampleNewClient(t *testing.T) {
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
 	// Output: PONG <nil>
+	err = client.Set("key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := client.Get("key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exist
 }
