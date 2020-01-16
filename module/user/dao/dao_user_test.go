3fb7ce4c0dc71b3e0ac4cae9893b14a8a04b7149
lhh <776334655@qq.com>
2020-01-16 11:45:17 +0800

    日志模块调整

--- module/user/dao/dao_user_test.go
+++ module/user/dao/dao_user_test.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"github.com/krilie/lico_alone/common/config"
 	"github.com/krilie/lico_alone/common/utils/str_util"
+	"os"
 	"testing"
 )
 
 func TestDao_GetAllValidUserId(t *testing.T) {
+	t.Log(os.Getwd())
 	dao := NewDao(config.Cfg.DB)
 	strings, err := dao.GetAllValidUserId(context.Background())
 	t.Log(str_util.ToJsonPretty(strings), err)
