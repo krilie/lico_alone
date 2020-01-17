a378b9be454e6ebfdd042fd428c50ccab83c3803
lhh <776334655@qq.com>
2020-01-17 09:28:53 +0800

    日志模块调整

--- module/user/dao/dao_user_test.go
+++ module/user/dao/dao_user_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/krilie/lico_alone/common/config"
 	"github.com/krilie/lico_alone/common/utils/str_util"
+	"os"
 	"runtime"
 	"testing"
 )
@@ -16,6 +17,8 @@ func TestDao_GetAllValidUserId(t *testing.T) {
 }
 
 func TestGetFilename(t *testing.T) {
+	environ := os.Environ()
+	fmt.Println(str_util.ToJsonPretty(environ))
 	_, filename, _, _ := runtime.Caller(0)
 	fmt.Println("Current test filename: " + filename)
 }
