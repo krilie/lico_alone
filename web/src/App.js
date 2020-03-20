008af92981930e19cd3c4b71bf137dca8ec19f0c
lhh <776334655@qq.com>
2020-03-20 15:13:48 +0800

    web test

--- web/src/App.js
+++ web/src/App.js
@@ -8,9 +8,11 @@ function App() {
   return (
     <div className="App">
         <Layout className="layout">
-            <Content/>
+            <Content style={{ padding: '0 50px' }}>
+                <div className="site-layout-content">建设中</div>
+            </Content>
             <Footer style={{ textAlign: 'center' }}>
-                <a href="www.beian.miit.gov.cn">皖ICP备20003857号</a>
+                <a href="http://www.beian.miit.gov.cn" target="_blank"  rel="noopener noreferrer">皖ICP备20003857号</a>
             </Footer>
         </Layout>
     </div>
