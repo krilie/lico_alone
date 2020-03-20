81417fa06588e2ef4dc51b27acc7935a9a0a8181
lhh <776334655@qq.com>
2020-03-20 14:01:12 +0800

    web test

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Layout } from 'antd';
+
+const {  Content, Footer } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <p>hello react!!!</p>
+        <Layout className="layout">
+            <Content/>
+            <Footer style={{ textAlign: 'center' }}>
+                <a href="www.beian.miit.gov.cn">皖ICP备20003857号</a>
+            </Footer>
+        </Layout>
     </div>
   );
 }
