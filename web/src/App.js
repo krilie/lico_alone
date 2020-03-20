912440b7bb8f7e6d45e68dc24df0ab1e6a1ba52c
lhh <776334655@qq.com>
2020-03-20 18:40:47 +0800

    web test

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './App.css';
-import {Layout} from 'antd';
-
-const {Content, Footer} = Layout;
+import {Row,Col} from 'antd';
 
 function App() {
     return (
         <div className="App" style={{height: '100%'}}>
-            <Layout className="layout" style={{height: '100%'}}>
-                <Content className="site-layout" style={{padding: '0 50px', marginTop: 64, height: '100%'}}>
-                    <div className="site-layout-background" style={{padding: 24, minHeight: 380}}>
-                        建设中
-                    </div>
-                </Content>
-                <Footer style={{textAlign: 'center'}}>
+            <Row span={1}>
+                <Col span={24}>
+                    建设中
+                </Col>
+            </Row>
+            <Row span={22}>
+                <Col span={24}>col</Col>
+            </Row>
+            <Row span={1}>
+                <Col span={24}>
                     <a href="http://www.beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">皖ICP备20003857号</a>
-                </Footer>
-            </Layout>
+                </Col>
+            </Row>
         </div>
     );
 }
