7d4ee8ff6c30e42f267be09449713c85022c5df8
acorn <1197829331@qq.com>
2020-03-20 21:54:37 +0800

    版本号

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import './App.css';
-import {Row,Col} from 'antd';
+import Icplable from "./icp/icplable";
+import {Layout} from 'antd';
+
+const {Header, Footer, Content} = Layout;
 
 function App() {
     return (
         <div className="App" style={{height: '100%'}}>
-            <Row span={1}>
-                <Col span={24}>
-                    建设中
-                </Col>
-            </Row>
-            <Row span={22}>
-                <Col span={24}>col</Col>
-            </Row>
-            <Row span={1}>
-                <Col span={24}>
-                    <a href="http://www.beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">皖ICP备20003857号</a>
-                </Col>
-            </Row>
+            <Layout>
+                <Header>Header</Header>
+                <Content>Content</Content>
+                <Footer><Icplable/></Footer>
+            </Layout>
         </div>
     );
 }
---
+++ web/src/icp/icplable.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Icplable extends React.Component {
+    render() {
+        return (
+            <div>
+                <a title="皖ICP备20003857号" href="http://www.beian.miit.gov.cn" target="_blank" rel="nofollow">皖ICP备20003857号</a>
+            </div>
+        );
+    }
+}
