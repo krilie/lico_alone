3dea6d9a8a61be06b405c9b6fcbb9fea695a3ed7
lhh <776334655@qq.com>
2020-03-20 15:45:45 +0800

    web test

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './App.css';
-import { Layout } from 'antd';
+import {Layout} from 'antd';
 
-const {  Content, Footer } = Layout;
+const {Content, Footer} = Layout;
 
 function App() {
-  return (
-    <div className="App">
-        <Layout className="layout">
-            <Content style={{ padding: '0 50px' }}>
-                <div className="site-layout-content">建设中</div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>
-                <a href="http://www.beian.miit.gov.cn" target="_blank"  rel="noopener noreferrer">皖ICP备20003857号</a>
-            </Footer>
-        </Layout>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Layout className="layout">
+                <Content style={{padding: '0 50px', height: '100%'}}>
+                    <div className="site-layout-content">建设中</div>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>
+                    <a href="http://www.beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">皖ICP备20003857号</a>
+                </Footer>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
