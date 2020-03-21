5066b6ee760dfe20dfc656965b6e76799d73e210
acorn <1197829331@qq.com>
2020-03-21 22:55:26 +0800

    版本号

--- web/src/components/icp/icplable.js
+++ web/src/components/icp/icplable.js
@@ -4,7 +4,7 @@ export default class Icplable extends React.Component {
     render() {
         return (
             <div>
-                <a title="皖ICP备20003857号" href="http://www.beian.miit.gov.cn" target="_blank" rel="nofollow">皖ICP备20003857号</a>
+                <a title="皖ICP备20003857号" href="http://www.beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">皖ICP备20003857号</a>
             </div>
         );
     }
