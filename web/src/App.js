bc3d9cc565e58c9bc26995daef162399704f9581
acorn <1197829331@qq.com>
2020-03-22 16:31:21 +0800

    版本号

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
 import Icplable from "./components/icp/icplable";
-import {Layout} from 'antd';
-import Row from "antd/es/descriptions/Row";
-import Col from "antd/es/grid/col";
-
-const {Header, Footer, Content} = Layout;
 
 function App() {
     return (
         <div className="App" style={{height: '100%'}}>
-            <Row gutter={[8, 8]}>
-                <Col span={12} />
-                <Col span={12} />
-            </Row>
-            <Row gutter={[8, 8]}>
-                <Col span={12} />
-                <Col span={12} ><Icplable/></Col>
-            </Row>
+            <Icplable/>
         </div>
     );
 }
