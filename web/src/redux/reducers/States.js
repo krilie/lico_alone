c32278b2a95c2735f8a19d9f2047c460b0c17805
lhh <776334655@qq.com>
2020-04-20 21:11:07 +0800

    web test

--- web/src/redux/reducers/States.js
+++ web/src/redux/reducers/States.js
@@ -1,8 +1,19 @@
 //定义默认状态
 let initState = {
-    fileItems: {
-        dirPath: "",
-        items: []
-    },
+    todos: [
+        {
+            id: parseInt(Math.random() * 10000000),
+            isComplete: false,
+            title: '学习redux'
+        }, {
+            id: parseInt(Math.random() * 10000000),
+            isComplete: true,
+            title: '学习react'
+        }, {
+            id: parseInt(Math.random() * 10000000),
+            isComplete: false,
+            title: '学习node'
+        }
+    ]
 };
 export default initState;
