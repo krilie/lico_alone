a88f194f1a59462df63cbac530313955f6375c77
lhh <776334655@qq.com>
2020-04-22 09:33:53 +0800

    web path

--- web/src/api/UserApi.js
+++ web/src/api/UserApi.js
@@ -5,3 +5,8 @@ import {postMultiForm} from "./api";
 export function userLogin({name, password}) {
     return postMultiForm("/user/login", {name, password});
 }
+
+// 用户注册
+export function userRegister({phone,password,valid_code}) {
+    return postMultiForm("/user/register",{phone,password,valid_code});
+}
