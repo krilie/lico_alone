bec08da659c73602b59a1b77dbf65b24217edc39
acorn <1197829331@qq.com>
2020-05-03 22:32:08 +0800

    云存储

--- server/module/module-file/dao/dao_test.go
+++ server/module/module-file/dao/dao_test.go
@@ -1,11 +1,10 @@
 package dao
 
 import (
-	context2 "context"
-	"fmt"
 	"github.com/krilie/lico_alone/common/context"
 	"github.com/krilie/lico_alone/common/dig"
 	"github.com/krilie/lico_alone/common/utils/id_util"
+	"github.com/krilie/lico_alone/common/utils/str_util"
 	"github.com/krilie/lico_alone/module/module-file/model"
 	"testing"
 	"time"
@@ -13,18 +12,33 @@ import (
 
 func TestFileDao_CreateFile(t *testing.T) {
 	dig.Container.MustInvoke(func(dao *FileDao) {
-		dao.GetDb(context2.Background()).AutoMigrate(&model.FileMaster{})
-		err := dao.CreateFile(context.NewContext(), &model.FileMaster{
-			Id:          id_util.NextSnowflake(),
-			CreateTime:  time.Now(),
-			KeyName:     "22",
-			BucketName:  "33",
-			Url:         "44",
-			UserId:      "44",
-			ContentType: "55",
-			BizType:     "66",
-			Size:        7,
-		})
-		fmt.Println(err)
+		file := &model.FileMaster{Id: id_util.NextSnowflake(), CreateTime: time.Now(), KeyName: id_util.NextSnowflake(), BucketName: id_util.NextSnowflake(), Url: id_util.NextSnowflake(), UserId: "44", ContentType: "55", BizType: "66", Size: 7}
+		err := dao.CreateFile(context.NewContext(), file)
+		if err != nil {
+			t.Error("err db insert" + str_util.ToJsonPretty(file))
+			t.FailNow()
+		}
+		err = dao.DeleteFile(context.NewContext(), file.Id)
+		if err != nil {
+			t.Error("err db delete" + str_util.ToJsonPretty(file))
+			t.FailNow()
+		}
+	})
+}
+
+func BenchmarkNewFileDao(b *testing.B) {
+	dig.Container.MustInvoke(func(dao *FileDao) {
+		dao.GetDb(context.NewContext()).LogMode(false)
+		file := &model.FileMaster{Id: id_util.NextSnowflake(), CreateTime: time.Now(), KeyName: id_util.NextSnowflake(), BucketName: id_util.NextSnowflake(), Url: id_util.NextSnowflake(), UserId: "44", ContentType: "55", BizType: "66", Size: 7}
+		err := dao.CreateFile(context.NewContext(), file)
+		if err != nil {
+			b.Error("err db insert" + str_util.ToJsonPretty(file))
+			b.FailNow()
+		}
+		err = dao.DeleteFile(context.NewContext(), file.Id)
+		if err != nil {
+			b.Error("err db delete" + str_util.ToJsonPretty(file))
+			b.FailNow()
+		}
 	})
 }
--- server/module/module-user/dao/dao.go
+++ server/module/module-user/dao/dao.go
@@ -2,8 +2,10 @@ package dao
 
 import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/krilie/lico_alone/common/context"
 	"github.com/krilie/lico_alone/component/ndb"
 	"github.com/krilie/lico_alone/component/nlog"
+	"github.com/krilie/lico_alone/module/module-user/model"
 )
 
 type UserDao struct {
@@ -12,6 +14,14 @@ type UserDao struct {
 }
 
 func NewUserDao(db *ndb.NDb, log *nlog.NLog) *UserDao {
+
+	db.GetDb(context.NewContext()).AutoMigrate(
+		&model.Permission{},
+		&model.RolePermission{},
+		&model.Role{},
+		&model.UserRole{},
+		&model.UserMaster{})
+
 	return &UserDao{
 		log: log,
 		NDb: db,
