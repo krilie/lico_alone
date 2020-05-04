ef295a976b8205f8fdc5a431f2f9cc23c5fc8a69
acorn <1197829331@qq.com>
2020-05-04 23:10:52 +0800

    云存储

--- server/module/module-message/service/dig.go
+++ server/module/module-message/service/dig.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"github.com/krilie/lico_alone/common/dig"
+	"github.com/krilie/lico_alone/component/nlog"
+	"github.com/krilie/lico_alone/module/module-message/dao"
+	"github.com/krilie/lico_alone/module/module-message/infra/email"
+	"github.com/krilie/lico_alone/module/module-message/infra/sms"
+)
+
+func init() {
+	dig.Container.MustProvide(func(log *nlog.NLog, dao *dao.MessageDao, email email.IEmail, sms sms.IAliSms) *MessageService {
+		return NewMessageService(log, dao, email, sms)
+	})
+}
