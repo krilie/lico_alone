1a9bdc0c22232f0ceb4c63ce8ee64b844d450875
acorn <1197829331@qq.com>
2020-05-04 08:39:15 +0800

    云存储

--- server/module/module-file/service/service_broker.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import (
-	"context"
-)
-
-func (a *FileService) RegisterBroker(ctx context.Context) {
-
-}
--- server/module/module-file/service/service_file.go
+++ server/module/module-file/service/service_file.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"github.com/jinzhu/gorm"
 	"github.com/krilie/lico_alone/common/errs"
 	"github.com/krilie/lico_alone/common/utils/id_util"
 	"github.com/krilie/lico_alone/module/module-file/model"
@@ -12,29 +11,19 @@ import (
 )
 
 // 内部有事务的存在
-func (a *FileService) UploadFile(ctx context.Context, tx *gorm.DB, userId, fileName string, file multipart.File, size int) (url, bucket, key string, err error) {
+func (a *FileService) UploadFile(ctx context.Context, userId, fileName string, file multipart.File, size int) (url, bucket, key string, err error) {
 	err = a.dao.Transaction(ctx, func(ctx context.Context) error {
 		var content string
-		content, bucket, key, err = a.fileApi.UploadFile(ctx, userId, fileName, file, int64(size))
+		url, key, err = a.fileApi.UploadFile(ctx, fileName, file, int64(size))
 		if err != nil {
 			return err
 		}
-		item := model.FileMaster{
-			Id:          id_util.GetUuid(),
-			CreateTime:  time.Now(),
-			KeyName:     key,
-			BucketName:  bucket,
-			UserId:      userId,
-			ContentType: content,
-			BizType:     "",
-			Size:        size,
-		}
+		item := model.FileMaster{Id: id_util.GetUuid(), CreateTime: time.Now(), KeyName: key, BucketName: bucket, Url: url, UserId: userId, ContentType: content, BizType: "", Size: size}
 		err = a.dao.CreateFile(ctx, &item)
 		if err != nil {
 			log.Error(err.Error())
 			return errs.NewErrDbCreate().WithError(err)
 		}
-		url = a.fileApi.GetFullUrl(ctx, true, key)
 		return nil
 	})
 	return url, bucket, key, err
@@ -47,7 +36,7 @@ func (a *FileService) DeleteFile(ctx context.Context, bucket, key string) (err e
 		if err != nil {
 			return errs.NewErrDbDelete().WithError(err)
 		}
-		err = a.fileApi.DeleteFile(ctx, "", key)
+		err = a.fileApi.DeleteFile(ctx, key)
 		if err != nil {
 			return err
 		}
