8433cae5c38cc22242f48b3330a3ab2092e711ae
acorn <1197829331@qq.com>
2020-05-04 23:02:03 +0800

    云存储

--- server/server/cron/cron.go
+++ server/server/cron/cron.go
@@ -4,20 +4,12 @@ import (
 	"context"
 	"github.com/krilie/lico_alone/service"
 	"github.com/prometheus/common/log"
-	"github.com/robfig/cron/v3"
 )
 
-func mustAddCronFunc(cron *cron.Cron, spec string, f func()) {
-	_, err := cron.AddFunc(spec, f)
-	if err != nil {
-		panic(err)
-	}
-}
-
 func InitAndStartCorn(ctx context.Context, app *service.App) (cronStop func()) {
-	crone := cron.New()
+	crone := NewCrone()
 	//// 定时任务 * * 7 * * ?
-	mustAddCronFunc(crone, "0 0 7 * * *", func() {
+	crone.mustAddCronFunc("0 0 7 * * *", func() {
 		err := app.NotificationEmailService.SendGoodMorningEmail(ctx)
 		if err != nil {
 			log.Error(err)
@@ -25,6 +17,6 @@ func InitAndStartCorn(ctx context.Context, app *service.App) (cronStop func()) {
 	})
 	// stop 定时任务
 	return func() {
-		crone.Stop()
+		crone.StopAndWait()
 	}
 }
