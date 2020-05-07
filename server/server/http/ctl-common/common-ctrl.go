7ef2aa635bafdfa9e79fc24d8f92c68b91fe29e5
acorn <1197829331@qq.com>
2020-05-07 20:21:15 +0800

    swagger doc

--- server/server/http/ctl-common/common-ctrl.go
+++ server/server/http/ctl-common/common-ctrl.go
@@ -14,6 +14,13 @@ func NewCommonCtrl(common *common_service.CommonService) *CommonCtrl {
 	return &CommonCtrl{CommonService: common}
 }
 
+// Health Icp信息
+// @Summary Icp信息
+// @Description Icp信息
+// @Tags Icp信息
+// @ID Icp信息
+// @Success 200 {string} model.IcpInfo "pong start time up time"
+// @Router /api/common/icp_info [post]
 func (common *CommonCtrl) GetIcpInfo(c *gin.Context) {
 	info := common.CommonService.GetIcpInfo(ginutil.MustGetAppCtx(c))
 	ginutil.ReturnData(c, info)
--- server/server/http/ctl-health-check/check.go
+++ server/server/http/ctl-health-check/check.go
@@ -8,13 +8,30 @@ import (
 
 var startTime = time.Now()
 
+// Health 健康检查
+// @Summary 健康检查
+// @Description 健康检查
+// @Tags 健康检查
+// @ID 健康检查
+// @Success 200 {string} string "hello"
+// @Router /health [post]
+func Hello(c *gin.Context) {
+	c.String(http.StatusOK, "hello")
+}
+
+// Health 健康检查2
+// @Summary 健康检查2
+// @Description 健康检查2
+// @Tags 健康检查2
+// @ID 健康检查2
+// @Success 200 {string} string "pong start time up time"
+// @Router /health [post]
+func Ping(c *gin.Context) {
+	c.String(http.StatusOK, "pong start time "+startTime.String())
+}
+
 // 健康检查 ping
 func Init(engine *gin.Engine) {
-	engine.GET("health/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello")
-	})
-
-	engine.GET("health/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong start time "+startTime.String())
-	})
+	engine.GET("health/", Hello)
+	engine.GET("health/ping", Ping)
 }
