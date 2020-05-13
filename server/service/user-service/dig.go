c132113eedef88aa040291b50c8918730c0ecf1a
acorn <1197829331@qq.com>
2020-05-13 20:31:02 +0800

    web edit

--- server/service/user-service/dig.go
+++ server/service/user-service/dig.go
@@ -3,12 +3,13 @@ package user_service
 import (
 	"github.com/krilie/lico_alone/common/dig"
 	"github.com/krilie/lico_alone/component/nlog"
+	ConfigService "github.com/krilie/lico_alone/module/module-config/service"
 	MessageService "github.com/krilie/lico_alone/module/module-message/service"
 	"github.com/krilie/lico_alone/module/module-user/service"
 )
 
 func init() {
-	dig.Container.MustProvide(func(log *nlog.NLog, moduleUser *service.UserService, moduleMsg *MessageService.MessageService) *UserService {
-		return NewUserService(log, moduleUser, moduleMsg)
+	dig.Container.MustProvide(func(log *nlog.NLog, moduleConfig *ConfigService.ConfigService, moduleUser *service.UserService, moduleMsg *MessageService.MessageService) *UserService {
+		return NewUserService(log, moduleConfig, moduleUser, moduleMsg)
 	})
 }
