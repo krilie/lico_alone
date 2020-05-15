128a855aec309788c4737ac06aae2f3758e19b8a
acorn <1197829331@qq.com>
2020-05-15 22:25:45 +0800

    web edit

--- server/server/http/router.go
+++ server/server/http/router.go
@@ -2,7 +2,7 @@ package http
 
 import (
 	"context"
-	"github.com/gin-contrib/cors"
+	"fmt"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 	"github.com/krilie/lico_alone/common/dig"
@@ -18,6 +18,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -27,10 +28,12 @@ func InitAndStartHttpServer(ctx context.Context, app *service.App) (shutDown fun
 	// 路径设置 根路径
 	RootRouter := gin.Default() // logger recover
 	// 跨域
-	var theCors = cors.DefaultConfig()
-	theCors.AllowAllOrigins = true
-	theCors.AddAllowHeaders("Authorization")
-	RootRouter.Use(cors.New(theCors))
+	//var theCors = cors.DefaultConfig()
+	//theCors.AllowAllOrigins = true
+	//theCors.AddAllowHeaders("Content-Type", "X-CSRF-Token", "Authorization", "Token")
+	//theCors.AllowCredentials = true
+	//RootRouter.Use(cors.New(theCors))
+	RootRouter.Use(Cors())
 	// 静态文件 图片等
 	RootRouter.StaticFile("/files", app.Cfg.FileSave.LocalFileSaveDir)
 	// swagger + gzip压缩
@@ -137,5 +140,41 @@ func Version(version, buildTime, gitCommit, goVersion string) func(c *gin.Contex
 			"git_commit": gitCommit,
 			"go_version": goVersion,
 		})
+	}
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method               //请求方法
+		origin := c.Request.Header.Get("Origin") //请求头部
+		var headerKeys []string                  // 声明请求头keys
+		for k, _ := range c.Request.Header {
+			headerKeys = append(headerKeys, k)
+		}
+		headerStr := strings.Join(headerKeys, ", ")
+		if headerStr != "" {
+			headerStr = fmt.Sprintf("access-control-allow-origin, access-control-allow-headers, %s", headerStr)
+		} else {
+			headerStr = "access-control-allow-origin, access-control-allow-headers"
+		}
+		if origin != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Origin", "*")                                       // 这是允许访问所有域
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE") //服务器支持的所有跨域请求的方法,为了避免浏览次请求的多次'预检'请求
+			//  header的类型
+			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
+			//				允许跨域设置																										可以返回其他子段
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar") // 跨域关键设置 让浏览器可以解析
+			c.Header("Access-Control-Max-Age", "172800")                                                                                                                                                           // 缓存请求信息 单位为秒
+			c.Header("Access-Control-Allow-Credentials", "false")                                                                                                                                                  //	跨域请求是否需要带cookie信息 默认设置为true
+			c.Set("content-type", "application/json")                                                                                                                                                              // 设置返回格式是json
+		}
+
+		//放行所有OPTIONS方法
+		if method == "OPTIONS" {
+			c.JSON(http.StatusOK, "Options Request!")
+		}
+		// 处理请求
+		c.Next() //	处理请求
 	}
 }
