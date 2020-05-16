36bd1473923c721b13ad26e91848570cb64c84f8
acorn <1197829331@qq.com>
2020-05-16 21:21:54 +0800

    权限

--- server/module/module-config/model/model.go
+++ server/module/module-config/model/model.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Config struct {
-	Name       string    `gorm:"column:name;size:255;primary_key"`
-	CreateTime time.Time `gorm:"column:create_time;type:datetime;not null" json:"create_time"`
-	Value      string    `gorm:"column:value;size:5000;not null"`
+	Name       string    `json:"name" gorm:"column:name;size:255;primary_key"`
+	CreateTime time.Time `json:"create_time" gorm:"column:create_time;type:datetime;not null" json:"create_time"`
+	Value      string    `json:"value" gorm:"column:value;size:5000;not null"`
 }
 
 func (Config) TableName() string {
