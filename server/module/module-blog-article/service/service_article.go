281c41ea2e16129dd2416d93b47565cbac0fdf42
acorn <117@e.com>
2020-06-02 21:05:50 +0800

    改造

--- server/module/module-blog-article/service/service_article.go
+++ server/module/module-blog-article/service/service_article.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"context"
+	"github.com/krilie/lico_alone/module/module-blog-article/model"
+)
+
+func (b *BlogArticleService) CreateArticle(ctx context.Context, article *model.Article) error {
+	return b.Dao.CreateArticle(ctx, article)
+}
+
+func (b *BlogArticleService) DeleteArticleById(ctx context.Context, id string) (bool, error) {
+	return b.Dao.DeleteArticleById(ctx, id)
+}
+
+func (b *BlogArticleService) UpdateArticle(ctx context.Context, article *model.Article) error {
+	return b.Dao.UpdateArticle(ctx, article)
+}
+
+func (b *BlogArticleService) UpdateArticleSample(ctx context.Context, article *model.UpdateArticleModel) error {
+	return b.Dao.UpdateArticleSample(ctx, article)
+}
+
+func (b *BlogArticleService) QueryArticleById(ctx context.Context, id string) (*model.Article, error) {
+	return b.Dao.QueryArticleById(ctx, id)
+}
