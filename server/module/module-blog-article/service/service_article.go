fae494b9b6840868829ff83edf3c050687712345
acorn <117@e.com>
2020-06-02 21:29:02 +0800

    分页查询

--- server/module/module-blog-article/service/service_article.go
+++ server/module/module-blog-article/service/service_article.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	common_model "github.com/krilie/lico_alone/common/com-model"
+	"github.com/krilie/lico_alone/common/errs"
 	"github.com/krilie/lico_alone/module/module-blog-article/model"
 )
 
@@ -23,4 +25,33 @@ func (b *BlogArticleService) UpdateArticleSample(ctx context.Context, article *m
 
 func (b *BlogArticleService) QueryArticleById(ctx context.Context, id string) (*model.Article, error) {
 	return b.Dao.QueryArticleById(ctx, id)
+}
+
+// 分页查询
+func (b *BlogArticleService) QueryArticleSamplePage(ctx context.Context, page common_model.PageParams, searchKey string) (totalPage, totalCount int, data []*model.QueryArticleModel, err error) {
+
+	page.CheckOkOrSetDefault()
+
+	data = make([]*model.QueryArticleModel, 0)
+	db := b.Dao.GetDb(ctx).Model(new(model.Article))
+	if searchKey != "" {
+		db = db.Or("title like ?", "%"+searchKey+"%")
+		db = db.Or("description like ?", "%"+searchKey+"%")
+	}
+	err = db.Count(&totalCount).Error
+	if err != nil {
+		return 0, 0, nil, errs.NewInternal().WithError(err)
+	}
+	if totalCount <= 0 {
+		return 0, 0, data, nil
+	}
+	totalPage = totalCount / page.PageSize
+	// 获取结果
+	db = db.Order("created_at desc")
+	db = db.Limit(page.PageSize).Offset((page.PageIndex - 1) * page.PageSize)
+	err = db.Find(&data).Error
+	if err != nil {
+		return 0, 0, nil, errs.NewInternal().WithError(err)
+	}
+	return totalPage, totalCount, data, nil
 }
