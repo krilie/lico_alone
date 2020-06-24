1c2a02341860426adcf790e95cf4508234513225
lhh <776334655@qq.com>
2020-06-24 17:56:40 +0800

    init

--- server/module/module-statistic/dao/dao_func.go
+++ server/module/module-statistic/dao/dao_func.go
@@ -1,7 +1,18 @@
 package dao
 
-import "context"
+import (
+	"context"
+	"github.com/krilie/lico_alone/common/com-model"
+	"github.com/krilie/lico_alone/module/module-statistic/model"
+	"time"
+)
 
-func (a *StatisticDao) AddStatVisitorLogs(ctx context.Context) {
-
+func (a *StatisticDao) AddStatVisitorLogs(ctx context.Context, item *model.AddStatVisitorLogsModel) error {
+	log := a.log.Get(ctx).WithFuncName("AddStatVisitorLogs")
+	a.GetDb(ctx).Model(new(model.StatVisitorLogs)).Save(&model.StatVisitorLogs{
+		Model:      com_model.Model{},
+		AccessTime: time.Time{},
+		Ip:         "",
+		TraceId:    "",
+	})
 }
