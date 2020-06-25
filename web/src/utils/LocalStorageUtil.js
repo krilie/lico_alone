403b7408ac3f5d18915b33e866e39d75ac2c2b4c
acorn <117@e.com>
2020-06-25 21:49:07 +0800

    carousel view

--- web/src/utils/LocalStorageUtil.js
+++ web/src/utils/LocalStorageUtil.js
@@ -17,7 +17,7 @@ export function GetUserToken() {
 export function GetCustomerTraceId() {
     let traceId = store.get(UserTraceId) ?? ""
     if (traceId === "") {
-        traceId = generateUUID
+        traceId = generateUUID()
         store.set(UserTraceId, traceId)
     }
     return traceId;
@@ -42,12 +42,16 @@ export function hasToken() {
  * generateUUID 生成UUID
  * @returns {string} 返回字符串
  */
-function generateUUID(){
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
-    });
+function generateUUID() {
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "";    //"" 引号里面可以加任意字符，代表拼接的意思，如果不加就是 纯32位支付
+
+    var uuid = s.join("");
     return uuid;
 }
