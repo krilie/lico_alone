3074d93e9520ce7d160c69d3d4e723d0012d8241
lhh <776334655@qq.com>
2020-07-18 15:05:04 +0800

    provider

--- server/component/nlog/dig.go
+++ server/component/nlog/dig.go
@@ -4,11 +4,19 @@ import (
 	"github.com/krilie/lico_alone/common/dig"
 )
 
+var Log *NLog
+
 func init() {
 	dig.Container.MustProvide(NewLogger)
+	dig.Container.MustInvoke(func(log *NLog) {
+		Log = log
+	})
 }
 
 // DigProvider provider
 func DigProvider() {
 	dig.Container.MustProvide(NewLogger)
+	dig.Container.MustInvoke(func(log *NLog) {
+		Log = log
+	})
 }
