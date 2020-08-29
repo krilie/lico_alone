f05ffd401f016f01708e9f447a6b776caeaaa1e3
lhh <776334655@qq.com>
2020-08-29 14:07:02 +0800

    home 跳转

--- web/src/components/home/ArticleListItem.js
+++ web/src/components/home/ArticleListItem.js
@@ -35,7 +35,7 @@ class ArticleListItem extends React.Component {
                             <Layout.Header style={{minHeight: "20px", maxHeight: "20px"}}
                                            className="article-layout-content-real">
                                 <a href={url} rel="noopener noreferrer" target="_blank">
-                                    <div style={{height: "unset", verticalAlign: "center",color: "#52820f"}}>
+                                    <div style={{height: "unset", verticalAlign: "center",fontWeight: 900,color: "#52820f"}}>
                                         {title}
                                     </div>
                                 </a>
