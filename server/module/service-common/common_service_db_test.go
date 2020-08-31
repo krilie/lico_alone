b95d26b63881d8aec719903999469f5710291fd7
lhh <776334655@qq.com>
2020-08-31 17:28:11 +0800

    home 跳转

---
+++ server/module/service-common/common_service_db_test.go
@@ -0,0 +1,19 @@
+// +build !auto_test
+
+package service_common
+
+import (
+	"testing"
+)
+
+func TestCommonService_GetArticleById(t *testing.T) {
+	//oriDb, err := gorm.Open(mysql.Open("root:acA123bXY639@tcp(lizo.top:3306)/myapp?charset=utf8mb4&parseTime=True&loc=Asia%2FShanghai"), &gorm.Config{})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//newDb, err := gorm.Open(mysql.Open("root:acA123bXY639@tcp(lizo.top:3306)/myapp_tmp?charset=utf8mb4&parseTime=True&loc=Asia%2FShanghai"), &gorm.Config{})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//oriDb.Model(&model.Article{}).Find()
+}
