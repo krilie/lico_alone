dc6e665b4941eb96fb8cc982f3f4b8bce082273d
lhh <776334655@qq.com>
2020-09-16 16:35:57 +0800

    minio

--- web/src/utils/ImageProxy.js
+++ web/src/utils/ImageProxy.js
@@ -3,7 +3,9 @@ export const imageProxy = 'https://imageproxy.lizo.top'
 
 export function replaceForImageProxy(oriUrl, paramStr) {
     // http(s)://host/key/key.jpg => https://imageproxy.host/paramStr/key/key.jpg
-    var split1 = oriUrl.split("//",2);
-    var split2 = split1[1]("/",2)
+    if (oriUrl.indexOf("minio.lizo.top") === -1)
+        return imageProxy + "/" + paramStr + "/" + oriUrl;
+    var split1 = oriUrl.split("//", 2);
+    var split2 = split1[1].split("/", 2)
     return imageProxy + "/" + paramStr + "/" + split2[1];
 }
