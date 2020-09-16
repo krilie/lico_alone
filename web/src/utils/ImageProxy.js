36bf628b962e986cd91da936dcf2a1b33f2a3b2b
lhh <776334655@qq.com>
2020-09-16 16:57:17 +0800

    minio 图片修改

--- web/src/utils/ImageProxy.js
+++ web/src/utils/ImageProxy.js
@@ -5,7 +5,6 @@ export function replaceForImageProxy(oriUrl, paramStr) {
     // http(s)://host/key/key.jpg => https://imageproxy.host/paramStr/key/key.jpg
     if (oriUrl.indexOf("minio.lizo.top") === -1)
         return imageProxy + "/" + paramStr + "/" + oriUrl;
-    var split1 = oriUrl.split("//", 2);
-    var split2 = split1[1].split("/", 2)
-    return imageProxy + "/" + paramStr + "/" + split2[1];
+    var split1 = oriUrl.split("minio.lizo.top", 2);
+    return imageProxy + "/" + paramStr + "/" + split1[1];
 }
