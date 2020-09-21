da0faea00420cfc2cd1e9b2658bdf3f4805a8c97
lhh <776334655@qq.com>
2020-09-21 13:19:21 +0800

    限速

--- server/server/http/middleware/rate_limit.go
+++ server/server/http/middleware/rate_limit.go
@@ -23,7 +23,7 @@ func RateLimit() gin.HandlerFunc {
 
 // RateLimit 限制网速
 func RequestOpsLimit() gin.HandlerFunc {
-	limitBucket := ratelimit.NewBucket(time.Millisecond*100, 3) // 0.1秒三次
+	limitBucket := ratelimit.NewBucket(time.Millisecond*100, 30) // 0.1秒三次
 	return func(c *gin.Context) {
 		// 限制速度
 		limitBucket.Wait(1)
