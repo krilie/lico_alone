17f3dfbb8b5423152ec1187ba67ee4f3bd78c1c4
lhh <776334655@qq.com>
2020-10-23 10:54:21 +0800

    linq golang

--- server/module/module-statistic/service/statistic_querys.go
+++ server/module/module-statistic/service/statistic_querys.go
@@ -2,42 +2,37 @@ package service
 
 import (
 	"context"
+	"github.com/ahmetb/go-linq/v3"
 	jsoniter "github.com/json-iterator/go"
 	"github.com/krilie/lico_alone/common/utils/str_util"
 	"github.com/krilie/lico_alone/module/module-statistic/model"
 )
 
+// QueryAllVisitorLonLat
+// 查询所有访问过系统的经纬度与城市
 func (a *StatisticService) QueryAllVisitorLonLat(ctx context.Context) ([]model.VisitorLonlatModel, error) {
-	var isInListRet = func(list []model.VisitorLonlatModel, lonlatModel model.VisitorLonlatModel) bool {
-		for _, visitorLonlatModel := range list {
-			if visitorLonlatModel.City == lonlatModel.City &&
-				visitorLonlatModel.Lon == lonlatModel.Lon &&
-				visitorLonlatModel.Lat == lonlatModel.Lat {
-				return true
-			}
-		}
-		return false
-	}
-
-	var list []struct {
+	// 获取数据
+	type Memo struct {
 		Memo string `json:"memo" gorm:"column:memo"`
 	}
+	var list []Memo
 	err := a.Dao.GetDb(ctx).Model(&model.StatVisitorLogs{}).Find(&list).Error
 	if err != nil {
 		return nil, err
 	}
 	a.log.Get(ctx).WithField("list_data", str_util.ToJson(list)).Info("get memo list")
+	// 整理数据
 	var listRet []model.VisitorLonlatModel
-	for i := range list {
-		var item = model.VisitorLonlatModel{
-			Lon:  jsoniter.Get([]byte(list[i].Memo), "lon").ToFloat64(),
-			Lat:  jsoniter.Get([]byte(list[i].Memo), "lat").ToFloat64(),
-			City: jsoniter.Get([]byte(list[i].Memo), "city").ToString(),
+	linq.From(list).Select(func(o interface{}) interface{} {
+		return o.(Memo).Memo
+	}).Select(func(o interface{}) interface{} {
+		return model.VisitorLonlatModel{
+			Lon:  jsoniter.Get([]byte(o.(string)), "lon").ToFloat64(),
+			Lat:  jsoniter.Get([]byte(o.(string)), "lat").ToFloat64(),
+			City: jsoniter.Get([]byte(o.(string)), "city").ToString(),
 		}
-		if !isInListRet(listRet, item) {
-			listRet = append(listRet, item)
-		}
-	}
+	}).Distinct().ToSlice(listRet)
+	// 返回数据
 	a.log.Get(ctx).WithField("memo_data", str_util.ToJson(listRet)).Info("get memo list")
 	return listRet, nil
 }
