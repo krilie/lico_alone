b27283e0f4158eb838fbff117b40cfc4e2cfe418
lhh <776334655@qq.com>
2020-10-29 11:44:25 +0800

    web-article

---
+++ web-article/src/Article.js
@@ -0,0 +1,14 @@
+import './Article.css';
+import React from 'react';
+
+export default class Article extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    Hello react
+                </header>
+            </div>
+        );
+    }
+}
--- web-article/src/index.js
+++ web-article/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Article from './Article';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Article />
   </React.StrictMode>,
   document.getElementById('root')
 );
