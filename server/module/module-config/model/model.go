6851040b97b76d4d0893cf69f281a82b7bac79e1
Administrator <1197829331@qq.com>
2020-11-19 13:35:04 +0800

    update

--- server/module/module-config/model/model.go
+++ server/module/module-config/model/model.go
@@ -7,7 +7,7 @@ import (
 type Config struct {
 	Name       string    `json:"name" gorm:"column:name;size:255;primary_key"`
 	CreateTime time.Time `json:"create_time" gorm:"column:create_time;type:datetime;not null" json:"create_time"`
-	Value      string    `json:"value" gorm:"column:value;size:5000;not null"`
+	Value      string    `json:"value" gorm:"column:value;type:text;not null"`
 }
 
 func (Config) TableName() string {
