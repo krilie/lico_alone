385b645663d7fc145ab88a9ffba080dd3a9c9441
Administrator <1197829331@qq.com>
2020-11-27 16:14:49 +0800

    share one test

--- server/module/module-dynamic-share/dao/dao_dynamic_share.go
+++ server/module/module-dynamic-share/dao/dao_dynamic_share.go
@@ -3,6 +3,7 @@ package dao
 import (
 	"context"
 	"github.com/krilie/lico_alone/common/errs"
+	"github.com/krilie/lico_alone/component/ndb"
 	"github.com/krilie/lico_alone/module/module-dynamic-share/model"
 )
 
@@ -27,4 +28,35 @@ func (a *DynamicShareDao) UpdateDynamicShare(ctx context.Context, u model.Update
 		return errs.NewNormal().WithMsg("no updated")
 	}
 	return nil
+}
+
+func (a *DynamicShareDao) QueryDynamicShare(ctx context.Context, param model.QueryDynamicShareModel) (*model.QueryDynamicShareResModel, error) {
+	// 开始工作
+	log := a.log.Get(ctx, "DynamicShareDao", "QueryDynamicShare")
+	log.Info("begin QueryDynamicShare")
+	defer log.Info("end QueryDynamicShare")
+	param.PageParams.CheckOkOrSetDefault()
+	// 查询
+	db := a.GetDb(ctx)
+	db = db.Model(new(model.DynamicShare))
+
+	if param.ContentLike != "" {
+		db.Where("content", ndb.Like(param.ContentLike))
+	}
+	countDb := db
+	dataDb := db.Order("sort desc")
+
+	data := make([]model.DynamicShare, 0)
+	count, page, err := ndb.PageGetData(countDb, dataDb, param.PageNum, param.PageSize, &data)
+	if err != nil {
+		log.WithError(err).Error("sql query err")
+		return nil, err
+	}
+	return &model.QueryDynamicShareResModel{
+		TotalPage:  page,
+		TotalCount: count,
+		PageNum:    param.PageNum,
+		PageSize:   param.PageSize,
+		Data:       data,
+	}, nil
 }
