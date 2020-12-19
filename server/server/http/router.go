f9a5849a464c6e383b315e0350faefd7b2f91530
Administrator <1197829331@qq.com>
2020-12-19 17:20:47 +0800

    router 调整

--- server/server/http/router.go
+++ server/server/http/router.go
@@ -78,14 +78,15 @@ func (h *HttpService) InitAndStartHttpService(ctx context.Context) (shutDown fun
 			}
 			return *origin
 		}
+		manageGroup.Use(h.middleware.Cors(manageOriginFunc))
 		// 不检查权限的分组
-		noCheckToken := manageGroup.Group("", h.middleware.Cors(manageOriginFunc))
+		noCheckToken := manageGroup.Group("")
 		noCheckToken.POST("/user/login", h.ctrl.userCtrl.UserLogin)
 		noCheckToken.POST("/user/register", h.ctrl.userCtrl.UserRegister)
 		noCheckToken.POST("/user/send_sms", h.ctrl.userCtrl.UserSendSms)
 
 		//检查权限的分组
-		checkToken := manageGroup.Group("", h.middleware.Cors(manageOriginFunc), h.middleware.CheckAuthToken())
+		checkToken := manageGroup.Group("", h.middleware.CheckAuthToken())
 		checkToken.GET("/user/init_app", h.ctrl.userCtrl.InitApp)
 		checkToken.GET("/manage/setting/get_setting_all", h.ctrl.userCtrl.ManageGetConfigList)
 		checkToken.POST("/manage/setting/update_config", h.ctrl.userCtrl.ManageUpdateConfig)
