01bee7b2c2d30d5e2159411bd715ed9d4458584d
livo <1197829331@qq.com>
2021-01-19 14:04:29 +0800

    bean util copy

--- server/module/module-test/dao/dao_base.go
+++ server/module/module-test/dao/dao_base.go
@@ -3,6 +3,7 @@ package dao
 import (
 	"context"
 	"errors"
+	sq "github.com/Masterminds/squirrel"
 	"github.com/krilie/lico_alone/common/utils/id_util"
 	"github.com/krilie/lico_alone/module/module-test/model"
 	"gorm.io/gorm"
@@ -33,6 +34,23 @@ func (t *TestArticleDao) CreateBaseOne(ctx context.Context, baseOne *model.BaseO
 	if baseOne.Id == "" {
 		baseOne.Id = id_util.GetUuid()
 	}
-	err := t.GetDb(ctx).Model(new(model.BaseOne)).Create(baseOne).Error
+	err := t.GetDb(ctx).Model(baseOne).Create(baseOne).Error
+	return err
+}
+
+func (t *TestArticleDao) DeleteBaseOneById2(ctx context.Context, id string) error {
+	sql, args, err := sq.Delete("tb_base_one").Where("id=?", id).ToSql()
+	if err != nil {
+		return err
+	}
+	t.log.Get(ctx).WithField("sql", sql).WithField("params", args).Debug("sql to exec")
+	err = t.GetDb(ctx).Exec(sql, args...).Error
+	return err
+}
+
+func (t *TestArticleDao) DeleteBaseOneById3(ctx context.Context, id string) error {
+	sql := "delete from tb_article_master where id=?"
+	t.log.Get(ctx).WithField("sql", sql).WithField("params", []interface{}{id}).Debug("sql to exec")
+	err := t.GetDb(ctx).Exec(sql, id).Error
 	return err
 }
