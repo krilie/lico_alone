d6fefd9bb3af7cff5db4352817e9701c1ef15861
livo <1197829331@qq.com>
2021-01-19 15:18:56 +0800

    生成代码 catchword

---
+++ server/module/module-catchword/dao/dao_base.go
@@ -0,0 +1,56 @@
+package dao
+
+import (
+	"context"
+	"errors"
+	sq "github.com/Masterminds/squirrel"
+	"github.com/krilie/lico_alone/common/utils/id_util"
+	"github.com/krilie/lico_alone/module/module-catchword/model"
+	"gorm.io/gorm"
+)
+
+func (t *CatchwordDao) GetCatchwordById(ctx context.Context, id string) (*model.Catchword, error) {
+	catchword := new(model.Catchword)
+	err := t.GetDb(ctx).First(catchword, "id=?", id).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return catchword, err
+}
+
+func (t *CatchwordDao) DeleteCatchwordById(ctx context.Context, id string) error {
+	return t.GetDb(ctx).Delete(&model.Catchword{}).Where("id=?", id).Error
+}
+
+func (t *CatchwordDao) UpdateCatchwordById(ctx context.Context, catchword *model.Catchword) error {
+	result := t.GetDb(ctx).Model(new(model.Catchword)).Select("*").Updates(catchword)
+	return result.Error
+}
+
+func (t *CatchwordDao) CreateCatchword(ctx context.Context, catchword *model.Catchword) error {
+	if catchword.Id == "" {
+		catchword.Id = id_util.GetUuid()
+	}
+	err := t.GetDb(ctx).Model(catchword).Create(catchword).Error
+	return err
+}
+
+func (t *CatchwordDao) DeleteCatchwordById2(ctx context.Context, id string) error {
+	sql, args, err := sq.Delete("tb_catchword").Where("id=?", id).ToSql()
+	if err != nil {
+		return err
+	}
+	t.log.Get(ctx).WithField("sql", sql).WithField("params", args).Debug("sql to exec")
+	err = t.GetDb(ctx).Exec(sql, args...).Error
+	return err
+}
+
+func (t *CatchwordDao) DeleteCatchwordById3(ctx context.Context, id string) error {
+	sql := "delete from tb_catchword where id=?"
+	t.log.Get(ctx).WithField("sql", sql).WithField("params", []interface{}{id}).Debug("sql to exec")
+	err := t.GetDb(ctx).Exec(sql, id).Error
+	return err
+}
---
+++ server/module/module-catchword/model/model_base.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"github.com/krilie/lico_alone/common/com-model"
+)
+
+type Catchword struct {
+	com_model.Model
+	Title   string `json:"title" gorm:"column:title;type:varchar(128);not null;default:''"`      // 消息
+	Content string `json:"content" gorm:"column:content;type:varchar(1024);not null;default:''"` // 图片地址
+}
+
+func (Catchword) TableName() string {
+	return "tb_catchword"
+}
