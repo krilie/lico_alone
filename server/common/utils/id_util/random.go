5b7b0910517594772a2d99f9328bf41c6cdd8733
livo <1197829331@qq.com>
2021-01-21 09:19:23 +0800

    random func

---
+++ server/common/utils/id_util/random.go
@@ -0,0 +1,28 @@
+package id_util
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func RandomStrWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func RandomStr(length int) string {
+	return RandomStrWithCharset(length, charset)
+}
+
+func RandomInt() int64 {
+	return rand.Int63()
+}
---
+++ server/common/utils/id_util/random_test.go
@@ -0,0 +1,20 @@
+package id_util
+
+import "testing"
+
+func TestRandomStrWithCharset(t *testing.T) {
+	println(RandomInt())
+	println(RandomInt())
+	println(RandomInt())
+	println(RandomInt())
+	println(RandomStr(1))
+	println(RandomStr(3))
+	println(RandomStr(9))
+	println(RandomStr(30))
+	println(RandomStr(300))
+	println(RandomStrWithCharset(1, "1232123"))
+	println(RandomStrWithCharset(10, "123456789"))
+	println(RandomStrWithCharset(100, "abc123"))
+	println(RandomStrWithCharset(23, "abc"))
+	println(RandomStrWithCharset(45, "ab"))
+}
