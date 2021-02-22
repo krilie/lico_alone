0f172f74f2d777d0d0b230bd264e1657addca52b
livo <1197829331@qq.com>
2021-02-22 09:11:12 +0800

    catchword query for common

--- server/http/router.go
+++ server/http/router.go
@@ -128,11 +128,11 @@ func (h *HttpService) InitAndStartHttpService(ctx context.Context) (shutDown fun
 		commonApi.POST("/common/article/mark/remove_like", h.ctrl.commonCtrl.RemoveMarkArticleLike)
 		commonApi.POST("/common/article/mark/remove_dislike", h.ctrl.commonCtrl.RemoveMarkArticleDisLike)
 		commonApi.GET("/common/carousel/query", h.ctrl.commonCtrl.QueryCarousel)
-		commonApi.GET("/common/version", h.ctrl.commonCtrl.Version)             // 版本号
-		commonApi.POST("/common/visited", h.ctrl.commonCtrl.WebVisited)         // WebVisited
-		commonApi.GET("/common/about_app", h.ctrl.commonCtrl.AboutApp)          // AboutApp
-		commonApi.GET("/common/picture/:id", h.ctrl.commonCtrl.GetSlidePicById) // AboutApp
-
+		commonApi.GET("/common/version", h.ctrl.commonCtrl.Version)                // 版本号
+		commonApi.POST("/common/visited", h.ctrl.commonCtrl.WebVisited)            // WebVisited
+		commonApi.GET("/common/about_app", h.ctrl.commonCtrl.AboutApp)             // AboutApp
+		commonApi.GET("/common/picture/:id", h.ctrl.commonCtrl.GetSlidePicById)    // AboutApp
+		commonApi.GET("/common/catchword/query", h.ctrl.commonCtrl.QueryCatchword) // AboutApp
 	}
 
 	// 开始服务
