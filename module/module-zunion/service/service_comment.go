08aa234ecfbc9f31096102ac745e2fa138ebb441
livo <1197829331@qq.com>
2021-07-15 15:23:18 +0800

    rlico 创建

--- module/module-zunion/service/service_comment.go
+++ module/module-zunion/service/service_comment.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/krilie/lico_alone/common/errs"
 	"github.com/krilie/lico_alone/module/module-zunion/model"
+	"time"
 )
 
 func (a *ZUnionModule) AddComment(ctx context.Context, comment *model.TbComment) error {
@@ -13,4 +14,16 @@ func (a *ZUnionModule) AddComment(ctx context.Context, comment *model.TbComment)
 		return errs.NewInternal().WithMsg(err.Error())
 	}
 	return nil
+}
+
+func (a *ZUnionModule) DeleteComment(ctx context.Context, id string) error {
+	exec := a.Dao.GetDb(ctx).Exec("update tb_comment set deleted_at=? where id=?", time.Now(), id)
+	if exec.Error != nil {
+		return errs.NewInternal().WithMsg(exec.Error.Error())
+	}
+	return nil
+}
+
+func (a *ZUnionModule) QueryComment(ctx context.Context, targetId string) ([]*model.TbComment, error) {
+	panic("not implement")
 }
