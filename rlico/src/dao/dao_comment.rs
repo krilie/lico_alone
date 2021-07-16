15cab0350acaa627afaea345aaaa8e2077c115fa
livo <1197829331@qq.com>
2021-07-16 11:43:22 +0800

    rlico 创建

--- rlico/src/dao/dao_comment.rs
+++ rlico/src/dao/dao_comment.rs
@@ -1,13 +1,19 @@
 use sqlx::Executor;
 use crate::model::TbComment;
 use crate::util::errors::LicoError;
+use async_std::sync::Arc;
 
-// pub struct CommentDao<'a> {
-//     pub executor: &'a Executor,
-// }
-//
-// impl<DB, E: Executor<Database=DB>> CommentDao<DB, E> {
-//     pub async fn add_comment(&self, comment: &mut TbComment) -> Option<LicoError> {
-//         self.executor.execute()
-//     }
-// }
+pub struct CommentDao<'a> {
+    pub executor: Arc<dyn Executor<'a>>,
+}
+
+impl<'a> CommentDao<'a> {
+    pub async fn add_comment(&mut self, comment: TbComment) -> Result<(), LicoError> {
+        let sql = r######"insert into
+                          tb_comment(id, created_at, updated_at, deleted_at, user_id, comment_id, target_id, content, like_count, dislike_count, is_check)
+                          VALUES(?,?,?,?,?,?,?,?,?,?,?)"######;
+        let _ = sqlx::query(sql).bind("wy").bind("123456").execute(&mut self.executor).await?;
+        Ok(())
+    }
+}
+
